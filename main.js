43caac9196c12184a675c3064ec1b37c2cd83a99
shodmonovjaloliddin <pulatovjaloliddin777@gmail.com>
2024-09-18 17:45:19 +0300

    htt

---
+++ main.js
@@ -0,0 +1,58 @@
+let result;
+
+result = '3' + 2;
+console.log(result);
+
+result = '3' + true;
+console.log(result);
+
+result = '3' + undefined;
+console.log(result);
+
+result = '3' + null;
+console.log(result);
+
+result = '4' - '2';
+console.log(result);
+
+result = '4' - 2;
+console.log(result);
+
+result = '4' * 2;
+console.log(result);
+
+result = '4' / 2;
+console.log(result);
+
+result = 'hello' - 'world';
+console.log(result);
+
+result = '4' - 'hello';
+console.log(result);
+
+result = '4' - true;
+console.log(result);
+
+result = 4 + true;
+console.log(result);
+
+result = 4 + false;
+console.log(result);
+
+result = 4 + null;
+console.log(result);
+
+result = 4 - null;
+console.log(result);
+
+result = 4 + undefined;
+console.log(result);
+
+result = 4 - undefined;
+console.log(result);
+
+result = true + undefined;
+console.log(result);
+
+result = null + undefined;
+console.log(result);
